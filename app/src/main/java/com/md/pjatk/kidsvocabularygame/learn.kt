adafe3d8de5e6db5e1a24ae5fefd12b1f516e3fc
Filip Machaj <s13033@pjwstk.edu.pl>
2019-01-03 02:59:17 +0100

    dodanie przechodzenia między widokami

---
+++ app/src/main/java/com/md/pjatk/kidsvocabularygame/learn.kt
@@ -0,0 +1,18 @@
+package com.md.pjatk.kidsvocabularygame
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class learn : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learn)
+    }
+    fun learnAnimals(view: View){
+        val learnAnimalsButton = Intent(this,learn2::class.java)
+        startActivity(learnAnimalsButton)
+    }
+}
---
+++ app/src/main/java/com/md/pjatk/kidsvocabularygame/learn2.kt
@@ -0,0 +1,17 @@
+package com.md.pjatk.kidsvocabularygame
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class learn2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learn2)
+    }
+
+
+}
+
