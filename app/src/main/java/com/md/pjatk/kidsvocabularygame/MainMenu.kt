643faaf7070ec56c177478927d83d3042ec68b34
jaruzzzelski <s13909@pjwstk.edu.pl>
2019-01-21 15:33:03 +0100

    Many many changes

--- app/src/main/java/com/md/pjatk/kidsvocabularygame/MainMenu.kt
+++ app/src/main/java/com/md/pjatk/kidsvocabularygame/MainMenu.kt
@@ -1,29 +1,31 @@
 package com.md.pjatk.kidsvocabularygame
 
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.*
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.*
+import android.content.Intent
+import android.media.MediaPlayer
 
 
 class MainMenu : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main_menu)
+        startService(Intent(this, MyService::class.java))
+
     }
     fun learn( view: View){
         val learnButton = Intent(this,learn::class.java)
         startActivity(learnButton)
     }
 
-    /*fun toastMe(view: View){
-        val myToast = Toast.makeText(this,"co tam kurwa?",Toast.LENGTH_SHORT)
-        myToast.show()
-
-    }*/
     fun quiz (view: View){
 
         val quizButton = Intent(this,quiz::class.java)
--- app/src/main/java/com/md/pjatk/kidsvocabularygame/parents.kt
+++ app/src/main/java/com/md/pjatk/kidsvocabularygame/parents.kt
@@ -1,16 +1,15 @@
 package com.md.pjatk.kidsvocabularygame
 
+import android.content.Intent
 import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Switch
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_parent.*
-import android.view.View
 
 class parents : AppCompatActivity() {
 
-    var isMusicOn = false
+    var isMusicOn = true
     var isLanduagePolish = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,10 +20,10 @@ class parents : AppCompatActivity() {
 
         //languageSwichHandling()
        // musicSwichHandling() // to jest alternatywne podejscie
-        val language_swich_state = findViewById<Switch>(R.id.LanguageSwich)
-        val music_swich_state = findViewById<Switch>(R.id.MusicSwich)
-        language_swich_state.setOnClickListener{
-            if (language_swich_state.isChecked){
+        val language_switch_state = findViewById<Switch>(R.id.LanguageSwich)
+        val music_switch_state = findViewById<Switch>(R.id.MusicSwich)
+        language_switch_state.setOnClickListener{
+            if (language_switch_state.isChecked){
 
                 changeLanguageToPolish()
 
@@ -33,13 +32,15 @@ class parents : AppCompatActivity() {
                 changeLanguageToEnglish()
             }
         }
-        music_swich_state.setOnClickListener{
-            if (music_swich_state.isChecked){
-                turnOnMusic()
+      music_switch_state.setOnClickListener{
+            if (music_switch_state.isChecked){
+                stopService(Intent(this, MyService::class.java))
+
 
             }
             else{
-                turnOffMusic()
+                startService(Intent(this, MyService::class.java))
+
             }
         }
 
@@ -94,31 +95,32 @@ class parents : AppCompatActivity() {
                 turnOffMusic()
             }
         }
-
-    }*/
-  var music: MediaPlayer = MediaPlayer.create(this, R.raw.ac)
-
-    fun turnOnMusic(){
-
-        music.seekTo(0)
-
-        music.start()
-
-        val myToast = Toast.makeText(this,"Music On",Toast.LENGTH_SHORT)
-        myToast.show()
-
-        isMusicOn = true
-
+}*/
     }
+/* var music: MediaPlayer = MediaPlayer.create(this, R.raw.ac)
 
-    fun turnOffMusic(){
+  fun turnOnMusic(){
 
-        music.stop()
-        music.seekTo(0)
-        val myToast = Toast.makeText(this,"Music Off",Toast.LENGTH_SHORT)
-        myToast.show()
-        isMusicOn = false
+      music.seekTo(0)
 
-    }
+      music.start()
+
+      val myToast = Toast.makeText(this,"Music On",Toast.LENGTH_SHORT)
+      myToast.show()
+
+      isMusicOn = true
+
+  }
+
+  fun turnOffMusic(){
+
+      music.stop()
+      music.seekTo(0)
+      val myToast = Toast.makeText(this,"Music Off",Toast.LENGTH_SHORT)
+      myToast.show()
+      isMusicOn = false
+
+  }
 
 }
+   }*/
